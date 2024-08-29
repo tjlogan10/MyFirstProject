d1cdfbcdb5b961f9945aaf51d5bd219a54e562f9
tjlogan10 <10tjlogan@gmail.com>
2024-08-29 10:52:02 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+    Travis Logan
+    C++ Fall 2024
+    Due: Sept. 3 2024
+    Lab 1, lab 1 intro to C++ programming
+    Lab description: This lab is a basic demonstration of C++ programming.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Orange" << std::endl;
+ 
+    return 0;
+}
